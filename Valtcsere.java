10f37ba2075dd7325a58b98ac7ed5429821cd2fc
TomiMan7 <36338517+TomiMan7@users.noreply.github.com>
2018-02-10 19:29:27 +0100

    Add files via upload

---
+++ Valtcsere.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package valtcsere;
+
+/**
+ *
+ * @author tomiman
+ */
+public class Valtcsere {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a = 47, b = 74;
+        
+        System.out.println("a:" + a + "   b:" + b);
+        
+        a ^= b;
+        
+        b ^= a;
+        
+        a ^= b;
+        
+         System.out.println("a:" + a + "   b:" + b);       
+        
+        
+    }
+    
+}

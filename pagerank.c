ebf550a9eade5bf02f75e5a6d95fc2437bc3c116
TomiMan7 <36338517+TomiMan7@users.noreply.github.com>
2018-02-15 19:03:37 +0100

    Add files via upload

---
+++ pagerank.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+
+void print(double tomb[], int db)
+{
+	for(int i = 0; i < db; i++)
+		printf("PageRank[%d]: %lf\n",i,tomb[i]);
+}
+
+double distance(double PR[], double PRv[], int n)
+{
+
+double osszeg = 0.0;
+
+	for(int i = 0; i < n; i++)
+		osszeg += (PRv[i] - PR[i]) * (PRv[i] - PR[i]);
+
+return sqrt(osszeg);
+}
+
+int main()
+{
+
+double L [4][4] = {
+		{0.0, 0.0, 1.0 / 3.0, 0.0},
+		{1.0, 1.0 / 2.0, 1.0 / 3.0, 1.0},
+		{0.0, 1.0 / 2.0, 0.0, 0.0},
+		{0.0, 0.0, 1.0 /3.0, 0.0}
+		  };
+double PR[4] = {0.0, 0.0, 0.0, 0.0};
+double PRv[4] = {1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0};
+
+
+for(;;)
+{
+
+	for(int i = 0; i < 4; i++)
+	{
+		PR[i] = 0.0;
+			for(int j = 0; j < 4; j++)
+				PR[i] += (L[i][j] * PRv[j]);
+
+	}
+	
+	if (distance(PR,PRv, 4) < 0.0000000001)
+		break;
+
+	for(int i = 0; i < 4; i++)
+		PRv[i] = PR[i];
+}
+
+print(PR, 4);
+
+return 0;
+}

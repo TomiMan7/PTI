ee99dbc62b0992b8c1bf590fb34190ee268a3b4b
TomiMan7 <36338517+TomiMan7@users.noreply.github.com>
2018-05-06 18:01:18 +0200

    Update valtcsere.cpp

--- valtcsere.cpp
+++ valtcsere.cpp
@@ -8,11 +8,28 @@ int main()
 int a = 4, b = 7;
 
 cout<<"a: "<<a<<" b: "<<b<<endl;
-
 a ^= b;
 b ^= a;
 a ^= b;
+cout<<"a: "<<a<<" b: "<<b<<endl;
+
+a = 4, b = 7;
+cout<<endl;
 
 cout<<"a: "<<a<<" b: "<<b<<endl;
+a = a - b;
+b = a + b;
+a = b - a;
+cout<<"a: "<<a<<" b: "<<b<<endl;
+
+a = 4, b = 7;
+cout<<endl;
+
+cout<<"a: "<<a<<" b: "<<b<<endl;
+a = a * b;
+b = a / b;
+a = a / b;
+cout<<"a: "<<a<<" b: "<<b<<endl;
+
 
 }
